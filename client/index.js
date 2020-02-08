d8bd629f2b397377eb8cdf7e4bed01dadda4632f
Jared Monk <admin@jaredmonkdigital.com>
2020-02-07 18:12:21 -0600

    commit for later completion

--- client/index.js
+++ client/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./style/style.css";
+
+import { IndexRoute, Route, Router, hashHistory } from "react-router";
+
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import App from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom";
+import SongCreate from "./components/SongCreate";
+import SongList from "./components/SongList";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
